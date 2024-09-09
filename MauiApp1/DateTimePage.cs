0cbddb5bedb71e178d17fede748a1efa2be5e2d9
opilane <opilane@HP-CZC2349HT2>
2024-09-09 10:05:42 +0300

    datepicker, timepicker

--- MauiApp1/DateTimePage.cs
+++ MauiApp1/DateTimePage.cs
@@ -1,22 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Microsoft.Maui.Layouts;
 
 namespace MauiApp1
 {
     public class DateTimePage : ContentPage
     {
-        public DateTimePage()
+        Label lbl;
+        DatePicker dp;
+        TimePicker tp;
+        AbsoluteLayout abs;
+        public DateTimePage(int k)
         {
-            Content = new StackLayout
+            lbl = new Label
             {
-                Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
+                BackgroundColor = Color.FromRgb(120, 44, 133),
+                Text = "Select date or time"
             };
+
+            dp = new DatePicker
+            {
+                MinimumDate = DateTime.Now.AddDays(-10),
+                MaximumDate = DateTime.Now.AddDays(10),
+                Format = "D"
+            };
+            dp.DateSelected += Select_date;
+
+            tp = new TimePicker
+            {
+                Time = new TimeSpan(12, 0, 0)
+            };
+            tp.PropertyChanged += Select_time;
+
+            abs = new AbsoluteLayout
+            {
+                Children = { lbl, dp, tp}
+            };
+            AbsoluteLayout.SetLayoutBounds(lbl, new Rect(10, 10, 200, 50));
+            AbsoluteLayout.SetLayoutBounds(dp, new Rect(0.2, 0.2, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            AbsoluteLayout.SetLayoutFlags(dp, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(tp, new Rect(0.2, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            AbsoluteLayout.SetLayoutFlags(tp, AbsoluteLayoutFlags.PositionProportional);
+            Content = abs;
+        }
+        private void Select_time(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            lbl.Text ="Selected time: " + tp.Time.ToString();
+        }
+        private void Select_date(object? sender, DateChangedEventArgs e)
+        {
+            lbl.Text= "Selected date: " + e.NewDate.ToString("F");
         }
     }
 }
